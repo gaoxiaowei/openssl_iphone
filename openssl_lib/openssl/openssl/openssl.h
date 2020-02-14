693d8a1e3688df0e6e75c0af472b12fdbf7aee8b
gaoxiaowei <gaoxiaowei@360.cn>
2020-02-14 19:25:35 +0800

    crete libaray

---
+++ openssl_lib/openssl/openssl/openssl.h
@@ -0,0 +1,13 @@
+//
+//  openssl.h
+//  openssl
+//
+//  Created by gaoxiaowei on 2020/2/14.
+//  Copyright © 2020 gaoxiaowei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface openssl : NSObject
+
+@end
---
+++ openssl_lib/openssl/openssl/openssl.m
@@ -0,0 +1,13 @@
+//
+//  openssl.m
+//  openssl
+//
+//  Created by gaoxiaowei on 2020/2/14.
+//  Copyright © 2020 gaoxiaowei. All rights reserved.
+//
+
+#import "openssl.h"
+
+@implementation openssl
+
+@end
